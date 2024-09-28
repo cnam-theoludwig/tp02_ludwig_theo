eae14e681e2b63f0b39768f864235d7af6898979
Théo LUDWIG <contact@theoludwig.fr>
2024-09-28 15:36:42 +0200

    feat: button directive

--- src/app/customer-form/customer-form.component.ts
+++ src/app/customer-form/customer-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core"
+import { ButtonDirective } from "../button/button.directive"
 
 @Component({
   selector: "app-customer-form",
   standalone: true,
-  imports: [],
+  imports: [ButtonDirective],
   templateUrl: "./customer-form.component.html",
   styleUrl: "./customer-form.component.css",
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import { fontFamily } from "tailwindcss/defaultTheme"
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./src/**/*.html"],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       colors: {
