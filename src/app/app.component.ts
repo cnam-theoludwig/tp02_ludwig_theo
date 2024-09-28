397d5a70c27289216085c1a4c60855c869975632
Théo LUDWIG <contact@theoludwig.fr>
2024-09-28 10:17:40 +0200

    feat: layout components, Header, Footer and Sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from "@angular/core"
 import { RouterOutlet } from "@angular/router"
 import { CustomerFormComponent } from "./customer-form/customer-form.component"
+import { CustomerListComponent } from "./customer-list/customer-list.component"
+import { FooterComponent } from "./footer/footer.component"
+import { HeaderComponent } from "./header/header.component"
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, CustomerFormComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    CustomerFormComponent,
+    FooterComponent,
+    CustomerListComponent,
+  ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
